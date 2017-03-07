138e00d4de098595a74953093c346f8c65a9d740
Tim Buesking <tsbkwd@mst.edu>
2017-03-06 21:48:53 -0600

    Added character screen activity info with help from @Caroline Tran.

--- app/src/main/java/attackontinytim/barquest/CharacterScreenActivity.java
+++ app/src/main/java/attackontinytim/barquest/CharacterScreenActivity.java
@@ -3,6 +3,7 @@ package attackontinytim.barquest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import attackontinytim.barquest.Database.Weapon;
 
@@ -17,6 +18,24 @@ public class CharacterScreenActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         hero = bundler.unbundleHero(bundle);
+
+        TextView Name = (TextView) findViewById(R.id.CharName);
+        TextView Exp = (TextView) findViewById(R.id.expstat);
+        TextView Money = (TextView) findViewById(R.id.moneystat);
+        TextView LvlStat = (TextView) findViewById(R.id.lvlstat);
+        TextView HPStat = (TextView) findViewById(R.id.hpstat);
+        TextView AtkStat = (TextView) findViewById(R.id.atkstat);
+        TextView DefStat = (TextView) findViewById(R.id.defstat);
+        TextView SpdStat = (TextView) findViewById(R.id.spdstat);
+
+        Name.setText(String.valueOf(hero.getName()));
+        Exp.setText(String.valueOf(hero.getXP()));
+        Money.setText(String.valueOf(hero.getMoney()));
+        LvlStat.setText(String.valueOf(hero.getLevel()));
+        HPStat.setText(String.valueOf(hero.getHP()));
+        AtkStat.setText(String.valueOf(hero.getAttack()));
+        DefStat.setText(String.valueOf(hero.getDefense()));
+        SpdStat.setText(String.valueOf(hero.getSpeed()));
     }
 
     @Override
@@ -40,4 +59,7 @@ public class CharacterScreenActivity extends AppCompatActivity {
         setResult(RESULT_OK,getIntent().putExtras(bundle));
         finish();
     }
+
+
+    
 }
