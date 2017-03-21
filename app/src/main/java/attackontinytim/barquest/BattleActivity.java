3f8bc3c6047bca4cab69d78e15ba4b6990260cbb
Numbreon <twostarrynights@gmail.com>
2017-03-20 20:48:19 -0500

    Add fonts for later use.

--- app/src/main/java/attackontinytim/barquest/BattleActivity.java
+++ app/src/main/java/attackontinytim/barquest/BattleActivity.java
@@ -33,6 +33,7 @@ public class BattleActivity extends AppCompatActivity /*implements Parcelable*/{
         Bundle bundle = getIntent().getExtras();
 
         hero = bundler.unbundleHero(bundle);
+        // enemy = new Monster(/*some sort of Cursor*/);
 
         battle = new Battle(hero);
 
@@ -55,7 +56,7 @@ public class BattleActivity extends AppCompatActivity /*implements Parcelable*/{
 
         MonName.setText(String.valueOf(battle.enemy.getName()));
         MonLvl.setText(String.valueOf(battle.enemy.getLevel()));
-        TotalMonHP.setText(String.valueOf(battle.enemy.getHP()));
+        TotalMonHP.setText(String.valueOf(battle.enemy.getHP())); // change this once you can construct a monster
         CurrMonHP.setText(String.valueOf(battle.enemy.getHP()));
 
         onClickButtonListener();
@@ -70,11 +71,25 @@ public class BattleActivity extends AppCompatActivity /*implements Parcelable*/{
         attack.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
-                        // insert Battle() functions here to do calculations and update accordingly
-                        Intent intent = new Intent("attackontinytim.barquest.BattleActivity");
-                        Bundle bundle = bundler.generateBundle(hero);
-                        intent.putExtras(bundle);
-                        startActivityForResult(intent, MAIN_RETURN_CODE);
+                        /**insert Battle() functions here to do calculations and update accordingly
+                         //for now, hero always attacks first - will change in the future */
+                        if(battle.enemy.getHP() > 0 && battle.hero.getHP() > 0) {
+                            if (battle.calc_hit() == Boolean.TRUE) {
+                                //crit calculations are automatically done in the calc_dmg() stage
+                                int damage = battle.calc_dmg();
+
+                                //subtract damage from monster's HP
+                                battle.enemy.setHP(battle.enemy.getHP() - damage);
+                                //enemy automatically attacks if they still have health left
+                                if (battle.enemy.getHP() > 0 && battle.hero.getHP() > 0)
+                                    battle.hero.setHP(battle.hero.getHP() - battle.enemy.getAttack());
+                            }
+
+                            Intent intent = new Intent("attackontinytim.barquest.BattleActivity");
+                            Bundle bundle = bundler.generateBundle(hero);
+                            intent.putExtras(bundle);
+                            startActivityForResult(intent, MAIN_RETURN_CODE);
+                        }
                     }
                 }
         );
