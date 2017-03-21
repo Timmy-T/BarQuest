59cc4fc2f212131c2db5bce9029d3d5808a530b6
Tim Buesking <tsbkwd@mst.edu>
2017-03-20 23:30:59 -0500

    Updated Scanner Activity to scan barcode.

--- app/src/main/java/attackontinytim/barquest/ScannerActivity.java
+++ app/src/main/java/attackontinytim/barquest/ScannerActivity.java
@@ -3,7 +3,12 @@ package attackontinytim.barquest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import attackontinytim.barquest.Database.Weapon;
 
@@ -19,15 +24,23 @@ public class ScannerActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         hero = bundler.unbundleHero(bundle);
+
+        new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
     }
 
+    // Get the results:
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != RESULT_CANCELED) {
-            switch (requestCode) {
-                default:
-                    end();
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                Log.d("HERE", result.getContents());
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
