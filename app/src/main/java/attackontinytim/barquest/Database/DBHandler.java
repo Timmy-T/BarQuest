6a8d959a9373fb856ed5a5c0016cb39f429da2a8
SgtHeadshot <stpfz6@mst.edu>
2017-04-24 13:17:20 -0500

    Consumable Item changes
    Changed consumable item from just having attribute EffectStrength to having an Effect for each of the four main stats: HP, Speed, Defense, and Attack. Rewrote the Repo to reflect the changes. Changed items being inserted into the database, and finally incremented the Database Version by 1, up to 17

--- app/src/main/java/attackontinytim/barquest/Database/DBHandler.java
+++ app/src/main/java/attackontinytim/barquest/Database/DBHandler.java
@@ -8,7 +8,7 @@ import attackontinytim.barquest.Hero;
 public class DBHandler extends SQLiteOpenHelper {
 
     // Database Version
-    private static final int DATABASE_VERSION = 16;
+    private static final int DATABASE_VERSION = 17;
     // Database Name
     private static final String DATABASE_NAME = "BarDatabase";
     // Contacts table name
