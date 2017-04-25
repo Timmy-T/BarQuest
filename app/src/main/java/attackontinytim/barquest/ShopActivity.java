7f151837f70d6a787231d8fa299dbfa71ae3921f
NBlood <Imallskillzy@gmail.com>
2017-04-25 14:22:56 -0500

    Updated more shopactivity logic

--- app/src/main/java/attackontinytim/barquest/ShopActivity.java
+++ app/src/main/java/attackontinytim/barquest/ShopActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import attackontinytim.barquest.Hero;
 
 import attackontinytim.barquest.Database.ConsumableItem;
 import attackontinytim.barquest.Database.ConsumableRepo;
 import attackontinytim.barquest.Database.InventoryRepo;
 import attackontinytim.barquest.Database.Weapon;
+import attackontinytim.barquest.Database.WeaponRepo;
 
 public class ShopActivity extends AppCompatActivity {
 
@@ -25,15 +28,20 @@ public class ShopActivity extends AppCompatActivity {
     //3 types of stat pots
     private int randStat1 = rand.nextInt(3)+1;
     //3 types of stat pots
-    private int randStat2 = rand.nextInt(3)+1;
+    private int randStat2 = rand.nextInt(6)+3;
+    //3 types of debuff pots
+    private int randStat3 = rand.nextInt(3)+1;
     //6 types of weapons
     private int randWeapon = rand.nextInt(6)+1;
+
+
+
 
     //Buttons~
     private static Button HP_Pot_button;
     private static Button Stat_pot_1_button;
     private static Button Stat_pot_2_button;
-    private static Button escape_pot_button;
+    private static Button Stat_pot_3_button;
     private static Button weapon_button;
 
     //Consumables
@@ -43,10 +51,26 @@ public class ShopActivity extends AppCompatActivity {
     //statpot1 = ConsumableRepo.getConsumableByName("Debug");
     //statpot1quantity = ConsumableRepo.getItemQuantity(statpot1) ;
 
-    //"" stat pot1
-    //"" stat pot 2
-    //"" escape pot
-    //"" weapon
+    private ConsumableItem hpPot;
+    private ConsumableItem statPot1;
+    private ConsumableItem statPot2;
+    private ConsumableItem statPot3;
+    private Weapon weapon;
+
+    private List<ConsumableItem> hpList;
+
+    private List<ConsumableItem> attackList;
+    private List<ConsumableItem> defenseList;
+    private List<ConsumableItem> speedList;
+    private List<ConsumableItem> statPotList;
+
+
+    private List<ConsumableItem> debuffAttackList;
+    private List<ConsumableItem> debuffDefenseList;
+    private List<ConsumableItem> debuffSpeedList;
+    private List<ConsumableItem> debuffPotList;
+
+    private List<Weapon> weaponList;
 
 
 
@@ -65,19 +89,43 @@ public class ShopActivity extends AppCompatActivity {
         TextView hp_pot_label = (TextView) findViewById(R.id.hp_pot_label);
         TextView stat_pot_1_label = (TextView) findViewById(R.id.stat_pot_1_label);
         TextView stat_pot_2_label = (TextView) findViewById(R.id.stat_pot_2_label);
-        TextView escape_pot_label = (TextView) findViewById(R.id.escape_pot_label);
+        TextView stat_pot_3_label = (TextView) findViewById(R.id.stat_pot_3_label);
         TextView weapon_label = (TextView) findViewById(R.id.weapon_label);
         TextView hp_pot_quantity = (TextView) findViewById(R.id.hp_pot_quanitty);
         TextView stat_pot_1_quantity = (TextView) findViewById(R.id.stat_pot_1_quantity);
         TextView stat_pot_2_quantity = (TextView) findViewById(R.id.stat_pot_2_quantity);
-        TextView escape_pot_quantity = (TextView) findViewById(R.id.escape_pot_quantity);
+        TextView stat_pot_3_quantity = (TextView) findViewById(R.id.stat_pot_3_quantity);
         TextView weapon_quantity = (TextView) findViewById(R.id.weapon_quanity);
+
+        //get random potions
+
+        hpList = ConsumableRepo.getConsumableByType("Heal");
+        attackList = ConsumableRepo.getConsumableByType("Attack Up");
+        defenseList = ConsumableRepo.getConsumableByType("Defense Up");
+        speedList = ConsumableRepo.getConsumableByType("speedList");
+        //private ConsumableItem[] statPotList = attackList + defenseList + speedList;
+        debuffSpeedList = ConsumableRepo.getConsumableByType("Monster Speed Debuff");
+        debuffDefenseList = ConsumableRepo.getConsumableByType("Monster Defense Debuff");
+        debuffAttackList = ConsumableRepo.getConsumableByType("Monster Attack Debuff");
+        //debuffPotList = debuff
+
+        weaponList = WeaponRepo.getAllItems();
+
+
+
+        hpPot = hpList[randHP];
+        statPot1 = statPotList[randStat1];
+        statPot2 = statPotList[randStat2];
+        statPot3 = statPotList[randStat3];
+
+
+
 
         //set labels to name of item + cost
         //hp_pot_label.setText("HI");
         //stat_pot_1_label.setText("Bye");
         //stat_pot_2_label.setText("asdf");
-        //escape_pot_label.setText("WHY");
+        //stat_pot_3_label.setText("WHY");
         //weapon_label.setText("no");
 
         //set quantities to their values
@@ -86,11 +134,12 @@ public class ShopActivity extends AppCompatActivity {
         hp_pot_quantity.setText(String.valueOf("666"));
         stat_pot_1_quantity.setText(String.valueOf("asdf"));
         stat_pot_2_quantity.setText(String.valueOf("coo"));
-        escape_pot_quantity.setText(String.valueOf("asdff"));
+        stat_pot_3_quantity.setText(String.valueOf("asdff"));
         weapon_quantity.setText(String.valueOf("asss"));
 
 
-
+        //joe = ConsumableRepo.getConsumableByName("coffee");
+        //InventoryRepo.getItemQuantity(joe);
     }
 
     // This is called when the activity is ended via result
