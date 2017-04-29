f7332e2e4faa7f7ff285be17cdef839435445b97
XRCreator <robertjones436@yahoo.com>
2017-04-28 20:50:40 -0500

    Completely broken on Reset Character.

--- app/src/main/java/attackontinytim/barquest/MainActivity.java
+++ app/src/main/java/attackontinytim/barquest/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import java.util.Calendar;
 import java.util.List;
 
 import junit.framework.Test;
@@ -79,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 heroTimers[i].setTime(myArray[i]);
             }
             hero.setScanTimers(heroTimers);
+
          }
     }
 
@@ -200,6 +203,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         dbHandler.resetData();
                         hero = HeroRepo.getHeroByName("HERO");
+                        List<Long> timerList = TimerRepo.getAllTimers("HERO");
+                        Long[] myArray = new Long[10];
+                        myArray = timerList.toArray(myArray);
+
+                        Timer[] heroTimers = new Timer[10];
+                        for (int i = 0; i < 10; i++) {
+                            heroTimers[i] = new Timer();
+                            heroTimers[i].setTime(myArray[i]);
+                        }
+                        hero.setScanTimers(heroTimers);
                     }
                 }
         );
